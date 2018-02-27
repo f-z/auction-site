1ecddd6c639780425d249e5b44c748089373113d
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-02-27 21:05:33 +0000

    php for retrieving categories in insert item form

---
+++ php/get_categories.php
@@ -0,0 +1,24 @@
+<?php 
+
+    require_once('connect_azure_db.php');
+
+   // Declaring an empty array to store the data we retrieve from the database in.
+    $data = array();
+
+    // Attempting to query database table and retrieve data.
+    try {
+       $stmt = $pdo->query('SELECT * FROM category
+        ORDER BY description');
+       
+       while($row = $stmnt->fetch(PDO::FETCH_OBJ)) {
+          // Assigning each row of data to associative array.
+          $data[] = $row;
+      }
+      // Returning data as JSON.
+      echo json_encode($data);
+    }
+    // Catching potential exceptions being thrown.
+    catch(PDOException $e) {
+       echo $e->getMessage();
+    }
+?>
