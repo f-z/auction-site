48a39f90f2e099a1a5d4af8d7623a591c3afb915
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-02-28 15:43:21 +0000

    retrieveing highest bid php

---
+++ php/retrieve_highest_bid.php
@@ -0,0 +1,29 @@
+<?php
+    require_once('connect_azure_db.php');
+
+    // Retrieving the posted data.
+    $json    =  file_get_contents('php://input');
+    $obj     =  json_decode($json);
+
+    $auctionID = filter_var($obj->auctionID, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
+
+      try {
+
+      	$sql = 'SELECT MAX(price) 
+        FROM `bid` 
+        WHERE auctionID = :auctionID';
+
+        $retrieveBid = $pdo->prepare($sql);
+        $retrieveBid->bindParam(':auctionID', $auctionID, PDO::PARAM_INT);
+      	$retrieveBid->execute();
+
+        $data = $retrieveBid ->fetch(PDO::FETCH_OBJ);
+        // Returning data as JSON.
+        echo json_encode($data);
+
+      }
+       catch (Exception $e) {
+    $error = $e->getMessage();
+      }
+
+?>
