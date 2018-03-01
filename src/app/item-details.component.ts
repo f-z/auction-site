e820f922b122818fc1391c0040d7f390072147e6
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-03-01 14:38:33 +0000

    countdown added on auction

--- src/app/item-details.component.ts
+++ src/app/item-details.component.ts
@@ -74,6 +74,7 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
       .subscribe((data: any) => {
         this.auction = data[0];
         this.getHighestBid(data[0].auctionID);
+        this.countDown(data[0].endTime);
       },
       (error: any) => {
         // If there is an error, return to main search page.
@@ -84,13 +85,54 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
     return null;
   }
 
+  countDown(auction_endTime):void{
+     // Set the date we're counting down to
+    var countDownDate = new Date(auction_endTime).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    if(days >= 1){
+      document.getElementById("countdown").innerHTML ="Time remaining: "+ days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";}
+    else if (hours >= 1){
+        document.getElementById("countdown").innerHTML ="Time remaining: " + hours + "h "
+        + minutes + "m " + seconds + "s ";}
+    else if (minutes >= 1){
+          document.getElementById("countdown").innerHTML ="Time remaining: "+ minutes + "m " + seconds + "s ";}
+    else if (seconds >= 1){
+          document.getElementById("countdown").innerHTML ="Time remaining: "+ seconds + "s ";
+    }
+    
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "Time remaining: EXPIRED";
+    }
+      }, 1000);
+  }
+
   getHighestBid(auctionID): void {
     const headers: any    = new HttpHeaders({ 'Content-Type': 'application/json' }),
           options: any    = { 'auctionID': auctionID },
           url: any        = 'https://php-group30.azurewebsites.net/retrieve_highest_bid.php';
 
     this.http.post(url, JSON.stringify(options), headers)
-      .subscribe((data: any) => {
+      .subscribe((data: any) =>  // Set the date we're counting down to
+       {
         if (data != null) {
           this.highestBid = data[0];
         }
@@ -103,6 +145,26 @@ export class ItemDetailsComponent implements OnInit, OnDestroy {
 
     return null;
   }
+
+ /* calculateTimeRemaining(auction_endTime): void{
+          const headers: any    = new HttpHeaders({ 'Content-Type': 'application/json' }),
+          options: any    = { 'endTime': auction_endTime },
+          url: any        = 'https://php-group30.azurewebsites.net/calculate_time_remaining.php';
+
+          this.http.post(url, JSON.stringify(options), headers)
+      .subscribe((data: any) => {
+        if (data != null) {
+          this.timeRemaining = data;
+        }
+      },
+      (error: any) => {
+        // If there is an error, return to main search page.
+        //this.openDialog('Oops! Something went wrong; redirecting you to safety...', '', false);
+      }
+    );
+
+    return null;
+  }*/
 
   openDialog(message: string, username: string, succeeded: boolean): void {
     const dialogRef = this.dialog.open(DialogComponent, {
