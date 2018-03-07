67b21af312b37c235173b3e370a18ace295439e8
Filip A. Zofakis <fil.zofakis@gmail.com>
2018-03-07 17:40:06 +0000

    small fixes

--- php/retrieve_user.php
+++ php/retrieve_user.php
@@ -4,15 +4,20 @@
     // Retrieving the posted data.
     $json    =  file_get_contents('php://input');
     $obj     =  json_decode($json);
+
+    // Sanitising URL supplied values.
     $userID = filter_var($obj->userID, FILTER_SANITIZE_NUMBER_INT);
 
     try {
         $stmnt = $pdo->prepare('SELECT username FROM user
            WHERE userID= :userID');
 
+-       // Binding the provided username to our prepared statement.
         $stmnt->bindParam(':userID', $userID, PDO::PARAM_INT);
         $stmnt->execute();
-        $data = $stmnt->fetch(PDO::FETCH_OBJ)) {
+
+        // Declaring an empty array to store the data we retrieve from the database in.
+        $data = $stmnt->fetch(PDO::FETCH_OBJ);
         echo json_encode($data);
     }
     catch(PDOException $e) {
