5cb7ad3971b9a9c4186b905e7478b65262ece6e2
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-03-07 17:15:52 +0000

    more work on feedback

---
+++ php/retrieve_user.php
@@ -0,0 +1,30 @@
+<?php
+    require_once('connect_azure_db.php');
+
+    // Retrieving the posted data.
+    $json    =  file_get_contents('php://input');
+    $obj     =  json_decode($json);
+
+    // Sanitising URL supplied values.
+    $userID = filter_var($obj->userID, FILTER_SANITIZE_NUMBER_INT);
+
+    try {
+        $stmnt = $pdo->prepare('SELECT username FROM user
+           WHERE userID= :userID');
+
+        // Binding the provided username to our prepared statement.
+        $stmnt->bindParam(':userID', $userID, PDO::PARAM_INT);
+
+        $stmnt->execute();
+ 
+        // Declaring an empty array to store the data we retrieve from the database in.
+        $data =  = $stmnt->fetch(PDO::FETCH_OBJ)) {
+
+        // Returning data as JSON.
+        echo json_encode($data);
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
+?>
