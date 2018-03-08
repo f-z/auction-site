c013b1101433f0be48cd36faef1f2a74be6fe479
Filip A. Zofakis <fil.zofakis@gmail.com>
2018-03-08 17:07:54 +0000

    fixes

--- php/notify_prev_bidder.php
+++ php/notify_prev_bidder.php
@@ -1,35 +1,30 @@
 <?php
   require_once('connect_azure_db.php');
   
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-//Load composer's autoloader
-require_once('./vendor/autoload.php');
-$mail = new PHPMailer(true);
-  // Retrieving the posted data.
-  $json    =  file_get_contents('php://input');
-  $obj     =  json_decode($json);
-  $auctionID = filter_var($obj->auctionID, FILTER_SANITIZE_NUMBER_INT);
-  $highestBidderID = filter_var($obj->prevBidderID, FILTER_SANITIZE_NUMBER_INT);
-  $newBuyerID = filter_var($obj->newBuyer, FILTER_SANITIZE_NUMBER_INT);
-  
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    // Load composer's autoloader.
+    require_once('./vendor/autoload.php');
+    $mail = new PHPMailer(true);
+    // Retrieving the posted data.
+    $json    =  file_get_contents('php://input');
+    $obj     =  json_decode($json);
+    $auctionID = filter_var($obj->auctionID, FILTER_SANITIZE_NUMBER_INT);
+    $highestBidderID = filter_var($obj->prevBidderID, FILTER_SANITIZE_NUMBER_INT);
+    $newBuyerID = filter_var($obj->newBuyer, FILTER_SANITIZE_NUMBER_INT);
+
     try {
-        
-        if ($highestBidderID == $newBuyerID) {
-            break;
-        } else {
-            
+        if (!$highestBidderID == $newBuyerID) {
             // Retrieving userID of previous highest bidder
-             $sql = 'SELECT firstName, email FROM user
-             WHERE userID = :buyerID';
-             $retrieveBidder = $pdo->prepare($sql);
-             $retrieveBidder->bindParam(':buyerID', $highestBidderID, PDO::PARAM_INT);
-             $retrieveBidder->execute();
-             $prevBidder = $retrieveBidder->fetch(PDO::FETCH_ASSOC);
+            $sql = 'SELECT firstName, email FROM user
+            WHERE userID = :buyerID';
+            $retrieveBidder = $pdo->prepare($sql);
+            $retrieveBidder->bindParam(':buyerID', $highestBidderID, PDO::PARAM_INT);
+            $retrieveBidder->execute();
+            $prevBidder = $retrieveBidder->fetch(PDO::FETCH_ASSOC);
              
-             $emailPrev = $prevBidder['email'];
-             $namePrev = $prevBidder['firstName'];
-        //      if (strcmp($emailCur, $emailPrev) != 0) {
+            $emailPrev = $prevBidder['email'];
+            $namePrev = $prevBidder['firstName'];
                 
             //Server settings
             $mail->isSMTP();
@@ -46,7 +41,7 @@ $mail = new PHPMailer(true);
                 'verify_peer' => false,
                 'verify_peer_name' => false,
                 'allow_self_signed' => true
-            )
+                )
             );
             $mail->Subject = 'UCL Databases';
             $mail->Debugoutput = 'html';
@@ -55,17 +50,14 @@ $mail = new PHPMailer(true);
             $mail->Subject = 'UCL Databases';
             $mail->Debugoutput = 'html';
             $mail->Body = 'Hi '.$namePrev.', 
-                             You were outbid! Do not hesitate to jump back in! We still have what you are looking for!';  
+                             You were outbid! Do not hesitate to jump back in! The item is still up for grabs!';  
                  
             $mail->send();
             //   echo json_encode($mail);
-             echo json_encode('Email to previous bidder has been sent!');
-            
+            echo json_encode('Email to previous bidder has been sent!');
         }
-     
-     
-} catch (Exception $e) {
-    echo json_encode('Message could not be sent. Mailer Error: ', $mail->ErrorInfo);
-    die();
-}
+    } catch (Exception $e) {
+        echo json_encode('Message could not be sent. Mailer Error: ', $mail->ErrorInfo);
+        die();
+    }
 ?>
