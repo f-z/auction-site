9d00f111d9e8c53eee157c0a555b448483c4fb68
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-03-14 13:03:48 +0000

    php for recommendations

---
+++ php/get_recommendations_from_auction.php
@@ -0,0 +1,43 @@
+<?php
+    require_once('connect_azure_db.php');
+
+    // Retrieving the posted data.
+    $json    =  file_get_contents('php://input');
+    $obj     =  json_decode($json);
+
+    // Sanitising URL supplied values.
+    $auctionID = filter_var($obj->auctionID, FILTER_SANITIZE_NUMBER_INT); 
+
+    try {
+        // Retrieving item.
+        $stmnt = $pdo->prepare('SELECT DISTINCT auction.*, item.*, COUNT(`user`.userID) AS userCount
+		FROM auction
+		JOIN item ON auction.itemID = item.itemID
+		JOIN viewings ON auction.auctionID = viewings.auctionID
+		JOIN `user` ON viewings.userID = `user`.userID
+		JOIN viewings AS viewings1 ON `user`.userID = viewings1.userID
+		JOIN auction AS auction1 ON viewings1.auctionID = auction1.auctionID
+		WHERE auction1.auctionID = 33 AND auction.auctionID != auction1.auctionID 
+		GROUP BY auction.auctionID
+		ORDER BY userCount DESC
+');
+       
+        // Binding the provided username to our prepared statement.
+        $stmnt->bindParam(':auctionID', $auctionID, PDO::PARAM_INT);
+
+        $stmnt->execute();
+
+        $data=array();
+            // Fetching the row.
+        while($row =  $stmnt->fetch(PDO::FETCH_OBJ)){
+        	$data[] = $row;
+        }
+     
+        // Returning data as JSON.
+        echo json_encode($data);
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
+?>
