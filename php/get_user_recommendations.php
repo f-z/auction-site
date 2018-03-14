fc6fc6227c4d4a1284025e6f53366615a1be9a0e
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-03-14 20:49:00 +0000

    typ fix

--- php/get_user_recommendations.php
+++ php/get_user_recommendations.php
@@ -12,8 +12,8 @@
     try{
 
 	$stmnt = $pdo->prepare('SELECT other_user_views.userID AS other_userID, COUNT(*) AS same_auction_count
-		FROM viewings AS other_user_views 
-	  	JOIN viewings AS current_user_views 
+		FROM viewing AS other_user_views 
+	  	JOIN viewing AS current_user_views 
 	    ON current_user_views.auctionID =  other_user_views.auctionID 
 	      AND current_user_views.userID = :userID
 	      	AND current_user_views.userID != other_user_views.userID
