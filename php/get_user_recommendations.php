366d1778a6949c296d689dbe8fad6d21f3123437
Samuel Bradshaw <zcapscb@ucl.ac.uk>
2018-03-14 15:23:59 +0000

    php sql

---
+++ php/get_user_recommendations.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+$sql = 'SELECT other_user_views.userID AS other_user_id ,COUNT(*) AS same_auction_count
+	FROM viewings AS other_user_views 
+  JOIN viewings AS current_user_views 
+    ON current_user_views.auctionID =  other_user_views.auctionID 
+      AND current_user_views.userID = :userID
+      	AND current_user_views.userID != other_user_views.userID
+    
+    GROUP BY other_user_views.userID
+	ORDER BY same_auction_count DESC'
+
+  ?>
