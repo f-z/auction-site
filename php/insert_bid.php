14ea4ecf8a8639085b5d20998bc056bb89d216cf
German Malsagov <german.aqtau@gmail.com>
2018-03-14 14:07:53 +0000

    notification for watchers

--- php/insert_bid.php
+++ php/insert_bid.php
@@ -29,21 +29,64 @@
     $insertBid->bindParam(':time', $time, PDO::PARAM_STR);  
     $insertBid->bindParam(':auctionID', $auctionID, PDO::PARAM_INT);
 
-    $insertBid->execute();
-    
-    // Sending email to current highest bidder
-    $mail->addAddress($email, $firstname);
+    if ($insertBid->execute()){
 
-    $mail->Subject = 'Risk Assessment Update (NHS Falls)';
-    
-    $mail->Body    = '
-    Hi '.$firstname.',
+      // Sending email to current highest bidder
+      $mail->addAddress($email, $firstname);
 
-    Thanks for placing a bid. You are the highest bidder on '.$time;  
+      $mail->Subject = 'Risk Assessment Update (NHS Falls)';
+      
+      $mail->Body    = '
+      Hi '.$firstname.',
 
-    $mail->send();
-    echo json_encode('Congratulations! Your bid has been added!');
-  } catch (Exception $e) {
-      $error = $e->getMessage(); 
+      Thanks for placing a bid. You are the highest bidder on '.$time;  
+
+      if ($mail->send()){
+        echo json_encode('Congratulations! Your bid has been added!');
+        $mailer->ClearAllRecipients(); // clear all
+      }
+      
+      // Search users who are watching this item
+      $searchWatchers = 'SELECT u.firstName, u.email, i.name, b.auctionID, b.buyerID, b.price
+        FROM user u
+        JOIN bid b ON b.buyerID = u.userID
+        AND b.price = 0
+        JOIN auction a ON a.auctionID = b.auctionID
+        AND a.auctionID = :auctionID
+        AND a.endTime > NOW()
+        JOIN item i ON a.itemID = i.itemID';
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':auctionID', $auctionID, PDO::PARAM_INT);
+        $stmt->execute();
+        $watchers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($watchers as $watcher) {
+
+          $watcher_firstname = $watcher['firstName'];
+          $watcher_email = $watcher['email'];
+          $item_name = $watcher['name'];
+          $auctionID = $watcher['auctionID'];
+          $watcher_buyerID = $watcher['buyerID'];
+
+          $mail->Subject = 'UCL Databases';
+          $mail->Debugoutput = 'html';
+          $mail->setFrom('uclbay.gc06@gmail.com', 'UCLBay');
+          $mail->addAddress($watcher_email, $watcher_firstname);
+          $mail->Subject = 'Someone made a bid on '.$item_name.'';
+          $mail->Debugoutput = 'html';
+          $mail->Body = 'Hi '. $watcher_firstname. ', 
+                            We are contacting you because you are watching '.$item_name.'. A new bid of '.$price.' was submitted!';
+
+          if ($mail->send()){
+            echo ('Congratulations! Watcher has been notified!');
+            $mailer->ClearAllRecipients(); // clear all
+          }
+
+        }
+      }
+    } catch (Exception $e) {
+      $error = $e->getMessage();
+      echo $error;
   }
 ?>
